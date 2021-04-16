fcba22f315196cfd7ed073f63123c7afbc701156
mlmargaretli6@gmail.com <>
2021-04-15 17:22:20 -0700

    x

--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@ let animating = false;
 let img;
 let imageCounter = 0;
 let final = false;
+let strokeWidth = 0;
+let noiseOffset = 0;
 
 function preload() {
   for (let i = 0; i <= 10; i++) {
@@ -24,7 +26,7 @@ function setup() {
   textSize(48);
   textStyle(BOLD);
   fill(100, 0, 0);
-  text("Draw endangered animals!", 0.25*width, 0.5*height);
+  text("Repopulate endangered animals!", 0.25*width, 0.5*height);
   pop();
 
   imageMode(CENTER);
@@ -52,6 +54,13 @@ function draw() {
   }
 
   if (mouseIsPressed) {
+
+    strokeWeight(strokeWidth);
+    noiseOffset += 0.15;
+    strokeWidth = noise(noiseOffset)*15;
+
+    stroke(map(mouseY, 0, windowHeight, 0, 255, true));
+
     array.push([mouseX, mouseY]);
     drawAnimal();
   }
@@ -71,7 +80,8 @@ function randomizer() {
   animating = false;
   if (endangered[0]) {
     clear();
-    background(80, 180, 80);
+    //random shades of green background
+    background(random(50, 150), 180, random(50, 150));
 
     randomIndexEndangered = int(random(endangered.length));
     image(endangered[randomIndexEndangered], width * 0.5, height * 0.5);
@@ -99,7 +109,8 @@ function keyTyped() {
 
 function drawAnimal() {
   beginShape(); //draw image in curvilinear lines
-  stroke(0, 0, 255);
+  //lines with different shades of blue
+  stroke(random(115, 240), random(115, 240), 255);
 
   for (let i = 0; i < array.length; i++) {
     curveVertex(array[i][0], array[i][1]);
@@ -108,9 +119,24 @@ function drawAnimal() {
 
   //draw the same image at a smaller scale
   push();
-  translate(0.6 * width, 0.4 * height);
+  translate(0.35 * width, 0.3 * height);
   beginShape();
-  stroke(255, 0, 0);
+  //lines with different shades of red
+  stroke(255, random(115, 240), random(115, 240));
+  scale(0.75);
+  for (let i = 0; i < array.length; i++) {
+    curveVertex(array[i][0], array[i][1]);
+  }
+
+  endShape();
+  pop();
+
+  //draw another image at a smaller scale
+  push();
+  translate(0.6 * width, 0.35 * height);
+  beginShape();
+  //lines with different shades of green
+  stroke(random(115, 240), 255, random(115, 240));
   scale(0.5);
   for (let i = 0; i < array.length; i++) {
     curveVertex(array[i][0], array[i][1]);
